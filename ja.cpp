9e22a8b116c270ef6db5a42a5e029472076303bd
kamkia <100965810+kamkia@users.noreply.github.com>
2022-03-08 20:59:38 +0100

    Create ja.cpp

---
+++ ja.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void gwiazdki ()
+{
+    int znak;
+    std::cout << "wpisz znak z klawiatury" << std::endl;
+    std::cin >> znak;
+    
+        for (int i=0; i<10; i++)
+        
+    
+        cout << znak << std::endl;
+        
+}
+
+int main()
+{
+    int znak;
+    std::cout << gwiazdki(znak);
+
+    return 0;
+}
